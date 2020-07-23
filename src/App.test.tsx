f9240326e1c82ceae22e02af8bbefdb212428254
Saber <sabersalhi16@gmail.com>
2020-07-23 23:08:46 +0200

    init gitlab ci

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/this the branch feature/i);
   expect(linkElement).toBeInTheDocument();
 });
